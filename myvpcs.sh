6ebc0fe60adbc0c243bfccc19d681b223b95cc25
root <root@ip-172-31-81-114.ec2.internal>
2020-10-03 14:01:37 +0000

    created for loop for multi region

--- myvpcs.sh
+++ myvpcs.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
-REGION=$1
+REGIONS=$@
+for REGION in $REGIONS
+do
+echo "Listing VPC in REGION $REGION"
 aws ec2 describe-vpcs --region $REGION | jq .Vpcs[].VpcId
+echo "---------------------------------------------------------"
+echo " Listing VPC CIDRBLOCK in REGION $REGION"
+aws ec2 describe-vpcs --region $REGION | jq .Vpcs[].CidrBlockAssociationSet[].CidrBlock
+done
